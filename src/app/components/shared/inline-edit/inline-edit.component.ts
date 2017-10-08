6237338b5da69c4075789fa4424858eab39ce60c
Joshua Szuslik <joshuaszuslik@gmail.com>
2017-10-08 11:01:17 -0500

    Update

---
+++ src/app/components/shared/inline-edit/inline-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-edit',
+  templateUrl: './inline-edit.component.html',
+  styleUrls: ['./inline-edit.component.css']
+})
+export class InlineEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
